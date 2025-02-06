1358e538292ee01684a47208d930840c379306ed
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2025-02-06 14:46:10 +0000

    Done

--- Nested If Else/MaxNum4.cpp
+++ Nested If Else/MaxNum4.cpp
@@ -3,36 +3,45 @@ using namespace std;
 
 int main()
 {
-	int a,b,c,d;
-	cout <<"Enter Vlue Of a =>";
-	cin>>a;
-	cout <<"Enter Vlue Of b =>";
-	cin>>b;
-	cout <<"Enter Vlue Of c =>";
-	cin>>c;
-	cout <<"Enter Vlue Of d =>";
-	cin>>d;
-	
-	
-	if(a>b){
-		if(a>c){
-			cout <<"A Is Max";
-		}
-		else
-		{
-			cout <<"C Is Max";
-		}
-	}
-	else
-	{
-		if(b>c){
-			cout <<"B Is Max";
-		}	
-		else
-		{
-			cout <<"C Is Max";
-		}
-	}
-	
-	return 0;
+    int a, b, c, d;
+    cout << "Enter Value Of a => ";
+    cin >> a;
+    cout << "Enter Value Of b => ";
+    cin >> b;
+    cout << "Enter Value Of c => ";
+    cin >> c;
+    cout << "Enter Value Of d => ";
+    cin >> d;
+
+    if (a > b) {
+        if (a > c) {
+            if (a > d) {
+                cout << "A Is Max";
+            } else {
+                cout << "D Is Max";
+            }
+        } else {
+            if (c > d) {
+                cout << "C Is Max";
+            } else {
+                cout << "D Is Max";
+            }
+        }
+    } else {
+        if (b > c) {
+            if (b > d) {
+                cout << "B Is Max";
+            } else {
+                cout << "D Is Max";
+            }
+        } else {
+            if (c > d) {
+                cout << "C Is Max";
+            } else {
+                cout << "D Is Max";
+            }
+        }
+    }
+
+    return 0;
 }
