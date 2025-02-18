363c676c684533397913a77ae129054c7cf4f38b
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2025-02-18 08:27:49 +0000

    done

---
+++ SpaceLoop/p8.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//1 2 3 4 5
+//  2 3 4 5
+//    3 4 5
+//      4 5
+//        5
+
+	for(int i=1; i<=5; i++)
+	{
+		for(int k=1; k<i; k++)
+		{
+			cout << "  ";
+		}
+		for(int j=i; j<=5; j++)
+		{
+			cout<< j << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
