053ed8857771d1852ec8b15dc31a043620ac1732
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2025-02-18 09:53:32 +0000

    okay

---
+++ SpaceLoop/p19.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//1 0 1 0 1
+//  0 1 0 1
+//    1 0 1
+//      0 1
+//        1
+
+
+	 for(int i=1; i<=5; i++)
+		{
+			for(int k=1; k<i; k++)
+			{
+				cout << "  ";
+			}
+			for(int j=i; j<=5; j++)
+			{
+				if(j%2==0)
+				{
+					cout<< "0 ";
+				}
+				else
+				{
+					cout<< "1 ";
+				}
+			}
+			cout << endl;
+		}
+
+	return 0;
+}
