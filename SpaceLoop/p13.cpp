e14dd8e5ea7bd9db207faa7b57dad9defeea2bda
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2025-02-18 08:36:29 +0000

    ok1

---
+++ SpaceLoop/p13.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//        A
+//      B A
+//    C B A
+//  D C B A
+//E D C B A
+
+
+	for(char i='A'; i<='E'; i++)
+	{
+		for(char k=i; k<'E'; k++)
+		{
+			cout << "  ";
+		}
+		for(char j=i; j>='A'; j--)
+		{
+			cout<< j << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
