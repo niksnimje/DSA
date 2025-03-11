d8c6e8ccd78c476c2699624d02c96af25021b52c
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2025-03-11 10:02:54 +0000

    Okay

--- OPP-2/Spots.cpp
+++ OPP-2/Spots.cpp
@@ -2,41 +2,42 @@
 using namespace std;
 
 class Sport{
-	public:
+	private :
 	int age;
 	string name,gameName,role;
+	
+	
+	public:
+	void input(int age, string name,string gameName, string role)
+	{
+		this->age=age;
+		this->name=name;
+		this->gameName=gameName;
+		this->role=role;
+	}
+	void output()
+	{
+		cout << "Name: "<< name << endl;
+		cout << "Age: "<< age << endl;
+		cout << "Game Name: "<< gameName << endl;
+		cout << "Role: "<< role << endl <<endl;
+		
+	}
+	
 };
 
 int main()
 {
 	int a=10;
-	Sport e1,e2,e3;
+	Sport p1,p2,p3;
 	
-	e1.age=18;
-	e1.name="Arjun";
-	e1.gameName="Cricket";
-	e1.role="Bolwer";
+	p1.input(18,"Arjun","Cricket","Blower");
+	p1.output();
 	
-	e2.age=19;
-	e2.name="Om";
-	e2.gameName="Cricket";
-	e2.role="keeper";
-	
-	e3.age=20000;
-	e3.name="Nikhil";
-	e3.gameName="Cricket";
-	e3.role="keeper";
-	
-	
-	
-	cout << "Name: "<<e1.name << endl;
-	cout << "role: "<<e1.role << endl <<endl;
-	
-	cout << "Name: "<<e2.name << endl;
-	cout << "role: "<<e2.role << endl <<endl;
-	
-	cout << "Name: "<<e3.name << endl;
-	cout << "role: "<<e3.role << endl ;
+	p2.input(20,"om","Cricket","keeper");
+	p2.output();
+	p3.input(18,"Nikhil","Cricket","Better");
+	p3.output();
 	
 	return 0;
 }
